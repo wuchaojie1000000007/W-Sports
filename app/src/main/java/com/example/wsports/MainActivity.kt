2f108ce5e28d8292be5bd3fb7b13a43f025c1e74
Chaojie Wu <wuchaojie925@gmail.com>
2021-04-15 14:31:53 +0800

    fix bottom navigation

--- app/src/main/java/com/example/wsports/MainActivity.kt
+++ app/src/main/java/com/example/wsports/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.wsports.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -21,12 +22,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
 
-    /*override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        //setSupportActionBar(binding.toolbar)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -38,24 +39,15 @@ class MainActivity : AppCompatActivity() {
                 R.id.accountFragment,
                 R.id.profileFragment,
                 R.id.mySportsFragment,
-                R.id.settingsAndPrivacyFragment,
-                R.id.helpCentreFragment,
-                R.id.listsFragment
             ), findViewById(R.id.drawer_layout)
         )
-        //setupActionBarWithNavController(navController, appBarConfiguration)
+
         findViewById<Toolbar>(R.id.toolbar).setupWithNavController(
             navController,
             appBarConfiguration
         )
         findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
-        //binding.bottomNavView.setupWithNavController(navController)
-    }*/
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-
-        setContentView(R.layout.activity_main)
+        findViewById<BottomNavigationView>(R.id.bottom_nav_view).setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
